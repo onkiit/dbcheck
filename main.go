1631d20aa97712fe04e1fb9d18d58ce94dfedb2f
Tikno <tikno.edu@gmail.com>
2018-11-12 13:32:31 +0700

    trying factory

--- main.go
+++ main.go
@@ -9,31 +9,24 @@ import (
 	"github.com/onkiit/dbcheck/check"
 )
 
-func DBVersion(db check.VersionCheck) {
-	version, err := db.Version()
-	if err != nil {
-		fmt.Println("print version", err)
-		return
+var Versions = map[string]func(host string) check.VersionCheck{
+	"mysql":      check.NewMysql,
+	"postgresql": check.NewPsql,
+	"mongo":      check.NewMongo,
+	"redis":      check.NewRedis,
+}
+
+func NewVersion(db string, host string) check.VersionCheck {
+	if _, ok := Versions[db]; ok {
+		return Versions[db](host)
 	}
-	fmt.Println(version)
+	return nil
 }
 
 func main() {
 	db := flag.String("db", "mysql", "Specify your database server")
 	host := flag.String("host", "root@tcp(localhost:3306)/test", "Specify your database connection URI depending your server")
 	flag.Parse()
-	var versionCheck check.VersionCheck
-	switch *db {
-	case "postgresql":
-		versionCheck = check.NewPsql(*host)
-	case "mongodb":
-		versionCheck = check.NewMongo(*host)
-	case "redis":
-		versionCheck = check.NewRedis(*host)
-	default:
-		versionCheck = check.NewMysql(*host)
-	}
-
-	DBVersion(versionCheck)
-
+	var version = NewVersion(*db, *host)
+	fmt.Println(version.Version())
 }
